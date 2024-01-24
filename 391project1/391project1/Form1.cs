5c8beaa5c4b15666f42cef90b1e249fbe0eae76b
scratch-passive <isond@mymacewan.ca>
2024-01-24 12:50:19 -0700

    Add course registration tab layout

--- 391project1/391project1/Form1.cs
+++ 391project1/391project1/Form1.cs
@@ -6,5 +6,40 @@ namespace _391project1
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void tabPage2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void tabControl1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
