addd874c4d2b9229259f8c8bdba45e8c3b20152c
akimbo-md <abmark88@gmail.com>
2024-02-06 09:46:58 -0700

    New interface
    We're not making malware no more

--- 391project1/391project1/obj/Debug/net6.0-windows/391project1.AssemblyInfo.cs
+++ 391project1/391project1/obj/Debug/net6.0-windows/391project1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("391project1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+aa821a4c801217afef6f279affc1ff544a5ba405")]
 [assembly: System.Reflection.AssemblyProductAttribute("391project1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("391project1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
